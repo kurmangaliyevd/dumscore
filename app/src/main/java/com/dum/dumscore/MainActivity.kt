bc48b21a4a6d2036e5a56dae06912e35b97adbc0
dumios <kurmangaliyevd@gmail.com>
2022-12-17 15:38:27 +0600

    Renamed Score to score Removed space

--- app/src/main/java/com/dum/dumscore/MainActivity.kt
+++ app/src/main/java/com/dum/dumscore/MainActivity.kt
@@ -7,7 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
-    val Score = 3
+    val score = 3
 }
