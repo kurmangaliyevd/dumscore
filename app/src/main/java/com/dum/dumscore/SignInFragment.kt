89c219e441ea5ec5be58f9bef42a28a321f4dd7c
kurmangaliyevd <102383447+kurmangaliyevd@users.noreply.github.com>
2022-12-28 11:09:02 +0600

    added SignInFragment

--- app/src/main/java/com/dum/dumscore/SignInFragment.kt
+++ app/src/main/java/com/dum/dumscore/SignInFragment.kt
@@ -7,14 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 
 class SignInFragment : Fragment() {
-     
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
-
 }
