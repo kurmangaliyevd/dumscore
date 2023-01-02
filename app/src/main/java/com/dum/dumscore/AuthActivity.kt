f3e83a3c9b2e4232b5711974a46d93af8184718b
kurmangaliyevd <102383447+kurmangaliyevd@users.noreply.github.com>
2023-01-02 22:57:34 +0600

    connected bottomSheetFragment to parentFragment

--- app/src/main/java/com/dum/dumscore/AuthActivity.kt
+++ app/src/main/java/com/dum/dumscore/AuthActivity.kt
@@ -2,17 +2,22 @@ package com.dum.dumscore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 import com.dum.dumscore.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
 
-lateinit var binding: ActivityAuthBinding
+    lateinit var binding: ActivityAuthBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        val welcomeFragment = WelcomeFragment()
+        val fm: FragmentManager = supportFragmentManager
+        fm.beginTransaction().add(R.id.activity_auth, welcomeFragment).commit()
 
     }
 }
--- app/src/main/java/com/dum/dumscore/WelcomeFragment.kt
+++ app/src/main/java/com/dum/dumscore/WelcomeFragment.kt
@@ -9,14 +9,27 @@ import com.dum.dumscore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
 
-    lateinit var binding: FragmentWelcomeBinding
+    lateinit var dialog: SignInFragment
 
+
+
+    lateinit var binding: FragmentWelcomeBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentWelcomeBinding.inflate(layoutInflater, container, false)
+
+        binding.signInBtn.setOnClickListener {
+            dialog = SignInFragment()
+            dialog.show(parentFragmentManager, "tag")
+        }
+
         return binding.root
     }
+
+
+
+
 }
